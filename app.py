a0bc66753d38e5bc790a361d8fec7709486df61f
romeo018 <pirates4@yandex.ru>
2025-08-13 17:05:14 +0500

    начал работать с модулями

---
+++ app.py
@@ -0,0 +1,6 @@
+import my_module
+
+
+print("Hello World")
+
+print(my_module.summa(10, 20))
---
+++ my_module.py
@@ -0,0 +1,8 @@
+print('Hello из модуля')
+
+PI = 3.14
+def summa(a: int, b: int) -> int:
+    '''
+    Функция суммирует два числа
+    '''
+    return a + b
