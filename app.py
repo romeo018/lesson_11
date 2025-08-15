710d9057923e17f5f885bd1f125d04efb3c21308
romeo018 <pirates4@yandex.ru>
2025-08-15 17:13:56 +0500

    new

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ import tkinter as tk
 from my_module import study_entry
 from study_radiobutton import study_radio
 from study_checkbox import study_checkbox
+from key_logger import keylogger
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Изучение элементов GUI")
     root.geometry("400x300")
+    
     tk.Button(root, text="Изучение полей ввода", command=study_entry).pack(pady=20)
     
     tk.Button(root, text="Изучение радиокнопок", command=study_radio).pack(pady=20)
     
     tk.Button(root, text="Изучение чекбоксов", command=study_checkbox).pack(pady=20)
+    
+    tk.Button(root, text="Захват кнопок", command=keylogger).pack(pady=20)
     
     root.mainloop()
--- key_logger.py
+++ key_logger.py
@@ -2,18 +2,22 @@ import keyboard
 import tkinter as tk
 from tkinter import scrolledtext
 
-def f():   
+def keylogger():  
     
     root = tk.Tk()
     root.title("Кейлогер")
-    root.geometry("400x300") 
+    root.geometry("400x300")
+    root.attributes("-topmost", True)
     
     text_box = scrolledtext.ScrolledText(root, width=50, height=15)
     text_box.pack(pady=10)
     
+    def on_key_press(event):
+        text_box.insert(tk.END, event.name)  # Добавляем название клавиши в конец текста
+        text_box.see(tk.END)  # Прокручиваем текстовое поле к последней строке
+       
+    keyboard.on_press(on_key_press)  # Слушаем нажатия клавиш
+    
+    text_box.insert(tk.END, "Кейлогер запущен. Нажмите Ctrl+C для остановки.\n")
+    
     root.mainloop()
-        
-
-if __name__ == "__main__":
-    print("Кейлогер запущен. Нажмите ESC для остановки.")
-    f()
