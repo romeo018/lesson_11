d0ca48a13f1f510f4d0d87849bff4e55e10ea152
romeo018 <pirates4@yandex.ru>
2025-08-20 00:44:36 +0500

    дебагинг

---
+++ debugging.py
@@ -0,0 +1,22 @@
+import logging
+import colorlog
+
+# logging.basicConfig(level=logging.DEBUG)
+
+# настройка цветного логирования
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+    '%(log_color)s%(levelname)s:%(name)s:%(message)s'))
+
+logger = colorlog.getLogger(__name__)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+# некритические ошибки
+logger.debug("Подробная информация для отладки")           # DEBUG
+logger.info("Информационное сообщение")
+
+# критические ошибки
+logger.warning("Предупреждение")                           # ERROR
+logger.error("Ошибка")
+logger.critical("Критическая ошибка")
