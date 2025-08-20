5d1fa233dd450c44fe09997783cdabd55084d524
romeo018 <pirates4@yandex.ru>
2025-08-20 16:44:40 +0500

    +

--- debugging.py
+++ debugging.py
@@ -14,10 +14,22 @@ logger.setLevel(logging.DEBUG)
 logger.propagate = False  # отключаем передачу в родительский логгер
 
 # некритические ошибки
-logger.debug("Подробная информация для отладки")           # DEBUG
-logger.info("Информационное сообщение")
+# logger.debug("Подробная информация для отладки")           # DEBUG
+# logger.info("Информационное сообщение")
 
-# критические ошибки
-logger.warning("Предупреждение")                           # ERROR
-logger.error("Ошибка")
-logger.critical("Критическая ошибка")
+# # критические ошибки
+# logger.warning("Предупреждение")                           # ERROR
+# logger.error("Ошибка")
+# logger.critical("Критическая ошибка")
+
+
+def add_numbers(a: int,b: int) -> int:
+    logger.info(f"Сложение чисел: {a} и {b}")
+    return a + b
+
+logger.debug("Программа запущена")
+result = add_numbers(5, 3)
+logger.info(f"Результат: {result}")
+
+result = add_numbers(10, 20)
+name = 'John'
